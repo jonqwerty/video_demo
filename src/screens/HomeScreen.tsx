f4272a9c404e306e3d42f8836fb3ff167f0f0852
Serhii B <s_blyshchak@ukr.net>
2024-04-23 23:21:20 +0300

    refactor for order lists

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -9,6 +9,7 @@ import ListOfSmallCards from '../components/ListOfSmallCards';
 import ListOfBigCards from '../components/ListOfBigCards';
 
 const HomeScreen: FC = () => {
+  const order = data.sectionOrder;
   return (
     <>
       <StatusBar
@@ -19,23 +20,38 @@ const HomeScreen: FC = () => {
       <View style={styles.container}>
         <Header />
         <ScrollView>
-          {data.movies.filter(item => !item.coming) ? (
-            <ListOfBigCards data={data.movies.filter(item => !item.coming)} />
-          ) : null}
-
-          {data.movies.filter(item => item.trendingNow) ? (
-            <ListOfSmallCards
-              data={data.movies.filter(item => item.trendingNow)}
-              title={'Trending Now'}
-            />
-          ) : null}
-
-          {data.movies.filter(item => item.top) ? (
-            <ListOfSmallCards
-              data={data.movies.filter(item => item.top)}
-              title={'Top Romance'}
-            />
-          ) : null}
+          {order.map((item, index) => {
+            switch (item) {
+              case 'genre':
+                return (
+                  <View key={index}>
+                    <ListOfBigCards
+                      data={data.movies.filter(item => !item.coming)}
+                    />
+                  </View>
+                );
+              case 'trendingNow':
+                return (
+                  <View key={index}>
+                    <ListOfSmallCards
+                      data={data.movies.filter(item => item.trendingNow)}
+                      title={'Trending Now'}
+                    />
+                  </View>
+                );
+              case 'top':
+                return (
+                  <View key={index}>
+                    <ListOfSmallCards
+                      data={data.movies.filter(item => item.top)}
+                      title={'Top Romance'}
+                    />
+                  </View>
+                );
+              default:
+                return null;
+            }
+          })}
         </ScrollView>
       </View>
     </>
