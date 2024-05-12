993d18f43f4affd54319a9679f4b46cd92afd280
Serhii B <s_blyshchak@ukr.net>
2024-05-12 22:19:04 +0300

    fix loader

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,12 +1,12 @@
-import {ActivityIndicator, StyleSheet, Text, View} from 'react-native';
+import {ActivityIndicator, StyleSheet, View} from 'react-native';
 import React, {FC} from 'react';
 
-import {ScreenHeight, ScreenWidth} from '../common/style';
+import {Colors, ScreenHeight, ScreenWidth} from '../common/style';
 
 const Loader: FC = () => {
   return (
     <View style={styles.container}>
-      <ActivityIndicator size="large" color="#00ff00" />
+      <ActivityIndicator size="large" color={Colors.blue} />
     </View>
   );
 };
@@ -17,7 +17,7 @@ const styles = StyleSheet.create({
   container: {
     position: 'absolute',
     zIndex: 5,
-    top: ScreenHeight / 2,
-    left: ScreenWidth / 2,
+    top: ScreenHeight / 2 - 50,
+    left: ScreenWidth / 2 - 20,
   },
 });
